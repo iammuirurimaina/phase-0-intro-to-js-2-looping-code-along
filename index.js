56037e2fe9825a913d49c90fb88f245b041ec834
ian <ian.maina@student.moringaschool.com>
2023-06-17 12:35:16 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+function writeCards (friends, event) {
+    const thankYouMessages = [];
+    for(let i = 0; i < friends.length; i++){
+        const message = (`Thank you, ${friends[i]}, for the wonderful ${event} gift!`)
+        console.log (message)
+        thankYouMessages.push(message)
+    }
+    return thankYouMessages
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+
+//countdown
+function countDown(number){
+    while (number >= 0){
+        console.log(number--)
+    }
+}
+countDown (10)
